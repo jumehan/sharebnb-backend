a4c6396357eb280371ba390d87325a07a53445b6
Julie Han <juliemenghan@gmail.com>
2022-10-25 23:18:31 -0400

    fixed s3 upload

--- helpers/s3.js
+++ helpers/s3.js
@@ -10,7 +10,7 @@ const s3Client  = new S3Client({
   region: 'us-east-1',
 });
 
-const BUCKET_NAME = process.env.BUCKETEER_BUCKET_NAME
+const BUCKET_NAME = process.env.BUCKET_NAME
 
 // const {
 //   BUCKET_NAME,
